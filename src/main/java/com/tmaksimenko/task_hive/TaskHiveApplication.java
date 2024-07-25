93fd3bd03d047f93f954106dff2a3e8e7c67db3e
tmaksimenko <timothymaksimenko@gmail.com>
2024-07-24 21:27:13 -0400

    Initial commit

---
+++ src/main/java/com/tmaksimenko/task_hive/TaskHiveApplication.java
@@ -0,0 +1,13 @@
+package com.tmaksimenko.task_hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHiveApplication.class, args);
+	}
+
+}
