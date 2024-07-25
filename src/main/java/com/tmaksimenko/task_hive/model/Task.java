11634fcb77e8fd912f8226262cf120ad62ad6e6a
tmaksimenko <timothymaksimenko@gmail.com>
2024-07-25 00:59:44 -0400

    optimized imports

--- src/main/java/com/tmaksimenko/task_hive/model/Task.java
+++ src/main/java/com/tmaksimenko/task_hive/model/Task.java
@@ -1,7 +1,6 @@
 package com.tmaksimenko.task_hive.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
