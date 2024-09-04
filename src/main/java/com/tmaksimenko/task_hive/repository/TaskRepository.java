98cd17d66a0cdf42d06491009985da4983e22604
tmaksimenko <timothymaksimenko@gmail.com>
2024-09-04 12:33:46 -0400

    added repository method to find by user

--- src/main/java/com/tmaksimenko/task_hive/repository/TaskRepository.java
+++ src/main/java/com/tmaksimenko/task_hive/repository/TaskRepository.java
@@ -1,9 +1,15 @@
 package com.tmaksimenko.task_hive.repository;
 
 import com.tmaksimenko.task_hive.model.Task;
+import com.tmaksimenko.task_hive.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository <Task, Long> {
+
+    List<Task> findByUser (User user);
+
 }
